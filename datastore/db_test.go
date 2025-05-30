e3fe18c640febdbc89a2ca35f7e63180728f9f89
vlad <vladvoytkiv321@gmail.com>
2025-05-30 19:38:03 +0300

    Add segments-based storage with merging and rotation

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -1,6 +1,8 @@
 package datastore
 
 import (
+	"path/filepath"
+	"strconv"
 	"testing"
 )
 
@@ -81,4 +83,145 @@ func TestDb(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestSegmentCreation(t *testing.T) {
+	dir := t.TempDir()
+
+	db, err := OpenWithSegmentSize(dir, 1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	for i := 0; i < 5; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := string(make([]byte, 200))
+		err := db.Put(key, value)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	expectedSegments := 2
+	actualSegments := countSegmentFiles(dir)
+	if actualSegments != expectedSegments {
+		t.Errorf("expected %d segments, got %d", expectedSegments, actualSegments)
+	}
+
+	for i := 0; i < 5; i++ {
+		key := "key" + strconv.Itoa(i)
+		value, err := db.Get(key)
+		if err != nil {
+			t.Errorf("failed to get key %s: %v", key, err)
+		}
+		if len(value) != 200 {
+			t.Errorf("expected value length 200, got %d", len(value))
+		}
+	}
+}
+
+func TestSegmentMerging(t *testing.T) {
+	dir := t.TempDir()
+
+	db, err := OpenWithSegmentSize(dir, 512)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	for i := 0; i < 10; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := string(make([]byte, 100))
+		err := db.Put(key, value)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	segmentsBeforeMerge := countSegmentFiles(dir)
+	if segmentsBeforeMerge < 3 {
+		t.Errorf("expected at least 3 segments before merge, got %d", segmentsBeforeMerge)
+	}
+
+	err = db.mergeSegments()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	segmentsAfterMerge := countSegmentFiles(dir)
+	if segmentsAfterMerge >= segmentsBeforeMerge {
+		t.Errorf("merge should reduce segments from %d to less, got %d", segmentsBeforeMerge, segmentsAfterMerge)
+	}
+
+	for i := 0; i < 10; i++ {
+		key := "key" + strconv.Itoa(i)
+		value, err := db.Get(key)
+		if err != nil {
+			t.Errorf("failed to get key %s after merge: %v", key, err)
+		}
+		if len(value) != 100 {
+			t.Errorf("expected value length 100 after merge, got %d", len(value))
+		}
+	}
+}
+
+func TestSegmentMergingWithUpdates(t *testing.T) {
+	dir := t.TempDir()
+
+	db, err := OpenWithSegmentSize(dir, 400)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	for i := 0; i < 5; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "value" + strconv.Itoa(i) + string(make([]byte, 50))
+		err := db.Put(key, value)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "updated" + strconv.Itoa(i) + string(make([]byte, 50))
+		err := db.Put(key, value)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err = db.mergeSegments()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 3; i++ {
+		key := "key" + strconv.Itoa(i)
+		value, err := db.Get(key)
+		if err != nil {
+			t.Errorf("failed to get updated key %s: %v", key, err)
+		}
+		if value[:7] != "updated" {
+			t.Errorf("expected updated value, got %s", value)
+		}
+	}
+
+	for i := 3; i < 5; i++ {
+		key := "key" + strconv.Itoa(i)
+		value, err := db.Get(key)
+		if err != nil {
+			t.Errorf("failed to get original key %s: %v", key, err)
+		}
+		if value[:5] != "value" {
+			t.Errorf("expected original value, got %s", value)
+		}
+	}
+}
+
+func countSegmentFiles(dir string) int {
+	files, _ := filepath.Glob(filepath.Join(dir, "segment-*"))
+	return len(files)
 }
